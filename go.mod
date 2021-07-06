47af2d7e1806f1d38d19dff6bcd56d6277e8fcbd
Eduard Miller <eidenar.miller@gmail.com>
2021-07-06 22:47:53 +0300

    Minor update

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module backend-mocker
 go 1.16
 
 require (
-	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/fsnotify/fsnotify v1.4.9
 	github.com/gorilla/mux v1.8.0
 )
--- main.go
+++ main.go
@@ -14,8 +14,8 @@ const ADDRESS = "127.0.0.1:8080"
 const JSON_FILE = "structure.json"
 
 type Route struct {
-	Url      string      `json: url`
-	Response interface{} `json: response`
+	Url      string      `json:"url"`
+	Response interface{} `json:"response"`
 }
 
 type ResponseFunc func(w http.ResponseWriter, r *http.Request)
@@ -29,6 +29,7 @@ func responseWrapper(route Route) ResponseFunc {
 
 		log.Println(r.Method, r.URL, string(body))
 
+		w.Header().Set("Access-Control-Allow-Headers", "*")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Content-Type", "application/json")
 
@@ -38,7 +39,9 @@ func responseWrapper(route Route) ResponseFunc {
 	return response
 }
 
-func loadPaths(router *mux.Router) {
+func loadPaths() *mux.Router {
+	router := mux.NewRouter()
+
 	jsonFile, err := os.Open(JSON_FILE)
 	if err != nil {
 		log.Fatal(err)
@@ -52,11 +55,53 @@ func loadPaths(router *mux.Router) {
 	for _, route := range result {
 		router.HandleFunc(route.Url, responseWrapper(route))
 	}
+
+	return router
 }
 
 func main() {
+	router := loadPaths()
+	server := http.Server{Addr: ADDRESS, Handler: router}
+
+	// watcher, err := fsnotify.NewWatcher()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer watcher.Close()
+
+	// done := make(chan bool)
+	// go func() {
+	// 	for {
+	// 		select {
+	// 		case event, ok := <-watcher.Events:
+	// 			if !ok {
+	// 				return
+	// 			}
+	// 			log.Println("event:", event)
+	// 			if event.Op&fsnotify.Write == fsnotify.Write {
+	// 				log.Println("modified file:", event.Name)
+	// 				log.Println("Restaring server...")
+
+	// 				server.Shutdown(context.Background())
+	// 				router := loadPaths()
+	// 				server.Handler = router
+	// 				server.ListenAndServe()
+	// 			}
+	// 		case err, ok := <-watcher.Errors:
+	// 			if !ok {
+	// 				return
+	// 			}
+	// 			log.Println("error:", err)
+	// 		}
+	// 	}
+	// }()
+	// err = watcher.Add(JSON_FILE)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
 	log.Println("Starting HTTP server on port 8080")
-	router := mux.NewRouter()
-	loadPaths(router)
-	log.Fatal(http.ListenAndServe(":8080", router))
+	// log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(server.ListenAndServe())
+	// <-done
 }
